e7086cdeddfe8ed9a6f69a6da38d896e462a0e64
Justas Sarapas <justas.sarapas@gmail.com>
2019-08-10 18:30:54 +1200

    Menu changes; hide/show menu depending on fullscreen (for Windows); possibility to fullscreen; prompts for entering urls for views

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 const electron = require('electron')
-const { BrowserView, BrowserWindow, app } = electron;
+const { BrowserView, BrowserWindow, app, Menu } = electron;
 var path = require('path');
+const prompt = require('electron-prompt');
 
-app.commandLine.appendSwitch('--enable-features', 'OverlayScrollbar')
-
-app.on('window-all-closed', function () {
-    app.quit()
-});
+const isMac = process.platform === 'darwin';
 
 app.on('ready', () => {
     let win = new BrowserWindow({
         title: "Quad Browser",
-        width: 1024, 
-        height: 800,
+        fullscreenable: true,
+        resizable: true,
         show: false,
-        icon: path.join(__dirname, 'assets/icons/png/64x64.png'),
-        autoHideMenuBar: true
+        icon: path.join(__dirname, 'assets/icons/png/64x64.png')
     });
+
+    win.setFullScreen(true);
+    win.setMenuBarVisibility(false);
 
     let view1 = new BrowserView();
     let view2 = new BrowserView();
     let view3 = new BrowserView();
     let view4 = new BrowserView();
     let views = [ view1, view2, view3, view4 ];
+    
+    view1.title = "Top left";
+    view2.title = "Top right";
+    view3.title = "Bottom left";
+    view4.title = "Bottom right";
 
     views.forEach((view) => {
         view.webContents.setAudioMuted(true);
@@ -36,19 +40,90 @@ app.on('ready', () => {
         view4.webContents.loadURL('https://www.nflgamepass.com/en/games/2019/redskins-browns-2019080855#highlights')
 
         updateSize();
-    })
+    });
+
+    const changeAddress = (view) => {
+        prompt({
+            title: `${view.title} address`,
+            label: 'Address:',
+            height: 150,
+            width: 400,
+            resizable: false,
+            value: 'https://',
+            inputAttrs: {
+                type: 'url'
+            }
+        }, win)
+        .then((result) => {
+            if(result !== null) {
+                view.webContents.loadURL(result);
+            }
+        })
+        .catch(console.error);
+    };
+
+    const template = [
+        // { role: 'appMenu' }
+        ...(isMac ? [{
+          label: app.getName(),
+          submenu: [
+            { role: 'about' },
+            { type: 'separator' },
+            { role: 'services' },
+            { type: 'separator' },
+            { role: 'hide' },
+            { role: 'hideothers' },
+            { role: 'unhide' },
+            { type: 'separator' },
+            { role: 'quit' }
+          ]
+        }] : []),
+        // { role: 'fileMenu' }
+        {
+          label: 'File',
+          submenu: [
+            isMac ? { role: 'close' } : { role: 'quit' }
+          ]
+        },
+        {
+            label: 'View',
+            submenu: [
+                { role: 'togglefullscreen' }
+            ]
+        },
+        {
+          label: 'Address',
+          submenu: [
+            { label: view1.title, click: () => { changeAddress(view1) } },
+            { label: view2.title, click: () => { changeAddress(view2) } },
+            { label: view3.title, click: () => { changeAddress(view3) } },
+            { label: view4.title, click: () => { changeAddress(view4) }}
+          ]
+        }
+      ]
+    
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
 
     const updateSize = () => {
         var windowSize = win.getBounds();
 
-        let viewWidth = windowSize.width / 2;
-        let viewHeight = windowSize.height / 2;
+        let viewWidth = Math.floor(windowSize.width / 2);
+        let viewHeight = Math.floor(windowSize.height / 2);
 
         view1.setBounds({ x: 0, y: 0, width: viewWidth, height: viewHeight });
         view2.setBounds({ x: viewWidth, y: 0, width: viewWidth, height: viewHeight });
         view3.setBounds({ x: 0, y: viewHeight, width: viewWidth, height: viewHeight });
         view4.setBounds({ x: viewWidth, y: viewHeight, width: viewWidth, height: viewHeight });
     };
+
+    win.on('enter-full-screen', () => {
+        win.setMenuBarVisibility(false);
+    });
+
+    win.on('leave-full-screen', () => {
+        win.setMenuBarVisibility(true);
+    });
 
     win.on('resize', () => {
         updateSize();
@@ -64,4 +139,10 @@ app.on('ready', () => {
     }, 10 * 60 * 1000);
 
     win.show();
+});
+
+app.commandLine.appendSwitch('--enable-features', 'OverlayScrollbar')
+
+app.on('window-all-closed', function () {
+    app.quit()
 });
