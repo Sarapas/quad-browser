541b18e8c8d0d3532e09a154ebf7b79ddec836ff
Justas Sarapas <justas.sarapas@gmail.com>
2019-09-14 12:18:39 +1200

    renamed images; layouts displaying nicely

--- src/Layouts.vue
+++ src/Layouts.vue
@@ -1,36 +1,51 @@
 <template>
-    <div class="app page container">
+    <div id="app" class="page">
         <h2>Layouts</h2>
         <hr />
         <div class="images">
-            <Layout v-for="(l, index) in layouts" :key="index" v-bind:layout="l" v-bind:current="current" v-on:pick="onPick" />
+          <div class="layout" v-bind:class="{ current: isCurrent('SINGLE') }" v-on:click="onPick('SINGLE')">
+            <img src="./assets/layout-img/SINGLE.jpg" />
+          </div>
+          <div class="layout" v-bind:class="{ current: isCurrent('DUAL') }" v-on:click="onPick('DUAL')">
+            <img src="./assets/layout-img/DUAL.jpg" />
+          </div>
+          <div class="layout" v-bind:class="{ current: isCurrent('TRI') }" v-on:click="onPick('TRI')">
+            <img src="./assets/layout-img/TRI.jpg" />
+          </div>
+          <div class="layout" v-bind:class="{ current: isCurrent('QUAD') }" v-on:click="onPick('QUAD')">
+            <img src="./assets/layout-img/QUAD.jpg" />
+          </div>
+          <div class="layout" v-bind:class="{ current: isCurrent('QUADH') }" v-on:click="onPick('QUADH')">
+            <img src="./assets/layout-img/QUADH.jpg" />
+          </div>
+          <div class="layout" v-bind:class="{ current: isCurrent('QUADV') }" v-on:click="onPick('QUADV')">
+            <img src="./assets/layout-img/QUADV.jpg" />
+          </div>
+          <div class="layout" v-bind:class="{ current: isCurrent('FIVEH') }" v-on:click="onPick('FIVEH')">
+            <img src="./assets/layout-img/FIVEH.jpg" />
+          </div>
+          <div class="layout" v-bind:class="{ current: isCurrent('FIVEV') }" v-on:click="onPick('FIVEV')">
+            <img src="./assets/layout-img/FIVEV.jpg" />
+          </div>
+          <div class="layout" v-bind:class="{ current: isCurrent('SIXH') }" v-on:click="onPick('SIXH')">
+            <img src="./assets/layout-img/SIXH.jpg" />
+          </div>
+          <div class="layout" v-bind:class="{ current: isCurrent('SIXV') }" v-on:click="onPick('SIXV')">
+            <img src="./assets/layout-img/SIXV.jpg" />
+          </div>
+          <!-- <div class="layout" v-bind:class="{ current: isCurrent('NINE') }" v-on:click="onPick('NINE')">
+            <img src="./assets/layout-img/9.jpg" />
+          </div> -->
         </div>
     </div>
 </template>
 
 <script>
-import Layout from './components/Layout.vue'
-
 export default {
   name: 'layouts',
-  components: {
-      Layout
-  },
-  props: [ 'current' ],
   data: function() {
     return {
-        layouts: [
-            { name: 'Single', img: '1.jpg' },
-            { name: 'Dual', img: '2.jpg' },
-            { name: 'Tri', img: '3.jpg' },
-            { name: 'Quad', img: '4.jpg' },
-            { name: 'QuadH', img: '3+1.jpg' },
-            { name: 'QuadV', img: '1+3.jpg' },
-            { name: 'FiveH', img: '5.jpg' },
-            { name: 'FiveV', img: '5-2.jpg' },
-            { name: 'SixH', img: '6.jpg' },
-            { name: 'SixV', img: '6-2.jpg' },
-        ]
+        current: ''
     };
   },
   methods: {
@@ -38,7 +53,10 @@ export default {
         const { ipcRenderer, remote } = window.require('electron');
         ipcRenderer.send('change-layout', layout);
         remote.getCurrentWindow().close();
-    }
+    },
+    isCurrent: function(layout) {
+        return this.current === this.layout;
+    },
   },
   mounted: function () {
     const { ipcRenderer, remote } = window.require('electron');
@@ -85,5 +103,40 @@ export default {
     margin-bottom: 15px;
     margin-left: 30px;
     font-size: 24px;
+}
+.layout {
+    position: relative;
+    margin: 15px;
+    height: 70px;
+    width: 120px;
+}
+.layout img {
+    height: 100%;
+    width: 120px;
+}
+.layout:after {
+    position:absolute;
+    opacity:0;
+    transition: all 0.5s;
+    -webkit-transition: all 0.5s;
+    content:'';
+    width:100%; 
+    height:100%;
+    top:0; left:0;
+    background:rgba(0,0,0,0.3);
+}
+.current:after {
+    position:absolute;
+    transition: all 0.5s;
+    -webkit-transition: all 0.5s;
+    content:'';
+    opacity: 1;
+    width:100%; 
+    height:100%;
+    top:0; left:0;
+    background:rgba(0,0,0,0.3);
+}
+.layout:hover:after {
+    opacity:1;
 }
 </style>
