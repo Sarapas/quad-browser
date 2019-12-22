d4c3b97c6f239f4f7e3244db54a163e79f2dc2e6
Justas Sarapas <justas.sarapas@gmail.com>
2019-12-22 21:38:42 +0200

    integrating react in progress

---
+++ src/MyReactComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class MyReactComponent extends React.Component {
+    render() {
+        return <h1>Simux</h1>
+    }
+}
+
+
+
+// const MyReactComponent = () => <h1>Simux</h1>;
+
+export default MyReactComponent
